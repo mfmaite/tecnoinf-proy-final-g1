0bcbde472d128d029a3679b446d4cb11a9af2c85
Leo <leonardovieras@gmail.com>
2025-10-20 20:35:11 -0300

    login y logout quedaron funcionando

--- mobile/app/(main)/home.tsx
+++ mobile/app/(main)/home.tsx
@@ -8,7 +8,7 @@ export default function HomeScreen() {
 
   const handleLogout = async () => {
     await logout();
-    router.replace("./(auth)/login");
+    router.replace("/(auth)/login");
   };
 
   return (

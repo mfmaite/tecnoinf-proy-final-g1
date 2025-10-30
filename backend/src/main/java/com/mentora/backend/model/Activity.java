4a9b69521d67ad55b3e039d4009406db85f4c505
Maite <martinezf.maite@gmail.com>
2025-10-30 15:33:06 -0300

    crea modelo activity

---
+++ backend/src/main/java/com/mentora/backend/model/Activity.java
@@ -0,0 +1,29 @@
+package com.mentora.backend.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Activity")
+public class Activity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private ActivityType type;
+
+  @Column(nullable = true)
+  private String description;
+
+  @Column(nullable = false)
+  private String link;
+
+  @Column(nullable = false)
+  private LocalDateTime createdDate = LocalDateTime.now();
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "userId", referencedColumnName = "ci")
+  private User user;
+}

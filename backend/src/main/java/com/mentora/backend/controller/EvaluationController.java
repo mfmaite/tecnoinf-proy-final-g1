0ccd65b74a71e59a59e4cbbae1b15664fa23230f
Maite <martinezf.maite@gmail.com>
2025-11-05 21:10:36 -0300

    crear respuesta a una evaluacion

--- backend/src/main/java/com/mentora/backend/controller/EvaluationController.java
+++ backend/src/main/java/com/mentora/backend/controller/EvaluationController.java
@@ -1,5 +1,97 @@
 package com.mentora.backend.controller;
 
-public class EvaluationController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.mentora.backend.responses.DtApiResponse;
+import com.mentora.backend.dt.DtEvaluation;
+import com.mentora.backend.service.EvaluationService;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.security.core.Authentication;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import com.mentora.backend.requests.CreateEvaluationSubmissionRequest;
+import java.io.IOException;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import com.mentora.backend.dt.DtEvaluationSubmission;
 
+@RestController
+@RequestMapping("/evaluations")
+public class EvaluationController {
+  private final EvaluationService evaluationService;
+
+  public EvaluationController(EvaluationService evaluationService) {
+    this.evaluationService = evaluationService;
+  }
+
+  @Operation(summary = "Obtener evaluación",
+    description = "Obtiene una evaluación por su ID",
+    security = @SecurityRequirement(name = "bearerAuth"))
+  @ApiResponse(responseCode = "200", description = "Evaluación obtenida correctamente")
+  @ApiResponse(responseCode = "404", description = "Evaluación no encontrada")
+  @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+  @GetMapping("/{evaluationId}")
+  public ResponseEntity<DtApiResponse<DtEvaluation>> getEvaluation(@PathVariable Long evaluationId) {
+    try {
+      DtEvaluation evaluation = evaluationService.getEvaluation(evaluationId);
+
+      return ResponseEntity.ok(new DtApiResponse<>(
+        true,
+        HttpStatus.OK.value(),
+        "Evaluación obtenida correctamente",
+        evaluation
+      ));
+    } catch (ResponseStatusException e) {
+      return ResponseEntity.status(e.getStatusCode()).body(new DtApiResponse<>(
+          false,
+          e.getStatusCode().value(),
+          e.getReason(),
+          null
+      ));
+    }
+  }
+
+  @Operation(summary = "Crea una respuesta a una evaluación",
+    description = "Crea una respuesta a una evaluación",
+    security = @SecurityRequirement(name = "bearerAuth"))
+  @ApiResponse(responseCode = "200", description = "Respuesta creada correctamente")
+  @ApiResponse(responseCode = "404", description = "Evaluación o usuario no encontrado")
+  @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+  @PostMapping(value = "/{evaluationId}/response", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @PreAuthorize("hasRole('ESTUDIANTE')")
+  public ResponseEntity<DtApiResponse<DtEvaluationSubmission>> createResponse(
+    @PathVariable
+    Long evaluationId,
+    @ModelAttribute CreateEvaluationSubmissionRequest req,
+    Authentication authentication
+  ) {
+    try {
+      String userCi = authentication.getName();
+      DtEvaluationSubmission response = evaluationService.createEvaluationSubmission(evaluationId, userCi, req);
+
+      return ResponseEntity.ok(new DtApiResponse<>(
+        true,
+        HttpStatus.OK.value(),
+        "Respuesta a evaluación creada correctamente",
+        response
+      ));
+    } catch (ResponseStatusException e) {
+      return ResponseEntity.status(e.getStatusCode()).body(new DtApiResponse<>(
+          false,
+          e.getStatusCode().value(),
+          e.getReason(),
+          null
+      ));
+    } catch (IOException e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new DtApiResponse<>(
+          false,
+          HttpStatus.INTERNAL_SERVER_ERROR.value(),
+          e.getMessage(),
+          null
+      ));
+    }
+
+  }
 }
--- backend/src/main/java/com/mentora/backend/service/EvaluationService.java
+++ backend/src/main/java/com/mentora/backend/service/EvaluationService.java
@@ -1,26 +1,47 @@
 package com.mentora.backend.service;
 
 import com.mentora.backend.repository.EvaluationRepository;
+import com.mentora.backend.dt.DtFileResource;
 import com.mentora.backend.dt.DtEvaluation;
+import com.mentora.backend.dt.DtEvaluationSubmission;
 import com.mentora.backend.model.Evaluation;
+import com.mentora.backend.model.EvaluationSubmission;
+import com.mentora.backend.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import com.mentora.backend.requests.CreateEvaluationSubmissionRequest;
+import com.mentora.backend.repository.UserRepository;
+import com.mentora.backend.repository.EvaluationSubmissionRepository;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EvaluationService {
   private final EvaluationRepository evaluationRepository;
   private final FileStorageService fileStorageService;
+  private final UserService userService;
+  private final UserRepository userRepository;
+  private final EvaluationSubmissionRepository evaluationSubmissionRepository;
 
   public EvaluationService(
       EvaluationRepository evaluationRepository,
-      FileStorageService fileStorageService
+      FileStorageService fileStorageService,
+      UserService userService,
+      UserRepository userRepository,
+      EvaluationSubmissionRepository evaluationSubmissionRepository
     ) {
     this.evaluationRepository = evaluationRepository;
     this.fileStorageService = fileStorageService;
+    this.userService = userService;
+    this.userRepository = userRepository;
+    this.evaluationSubmissionRepository = evaluationSubmissionRepository;
   }
 
-  public DtEvaluation getEvaluation(String evaluationId) {
+  public DtEvaluation getEvaluation(Long evaluationId) {
     Evaluation evaluation = evaluationRepository.findById(evaluationId)
       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Evaluación no encontrada"));
+
     return getDtEvaluation(evaluation);
   }
 
@@ -43,5 +64,64 @@ public class EvaluationService {
         signedUrl,
         e.getCreatedDate()
     );
-}
+  }
+
+  public DtEvaluationSubmission getDtEvaluationSubmission(EvaluationSubmission e) {
+    String signedUrl = null;
+    String fileUrl = e.getFileUrl();
+    if (fileUrl != null) {
+        if (fileUrl.startsWith("gs://")) {
+            signedUrl = fileStorageService.generateSignedUrl(fileUrl);
+        } else {
+            signedUrl = fileUrl;
+        }
+    }
+
+    return  new DtEvaluationSubmission(
+        e.getId(),
+        e.getFileName(),
+        signedUrl,
+        e.getNote(),
+        userService.getUserDto(e.getAuthor()),
+        getDtEvaluation(e.getEvaluation())
+    );
+  }
+
+  public DtEvaluationSubmission createEvaluationSubmission(Long evaluationId, String userCi, CreateEvaluationSubmissionRequest req) throws IOException {
+    if (req.getFile() == null && req.getSolution() == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Respuesta a evaluación requiere texto o archivo");
+    }
+
+    List<EvaluationSubmission> existingSubmission = evaluationSubmissionRepository.findByEvaluationIdAndAuthorCi(evaluationId, userCi);
+    if (existingSubmission.size() > 0) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ya existe una respuesta para esta evaluación, intenta editarla");
+    }
+
+    String fileName = null;
+    String fileUrl = null;
+
+    if (req.getFile() != null) {
+      DtFileResource file = fileStorageService.store(req.getFile());
+      fileName = file.getFilename();
+      fileUrl = file.getStoragePath();
+    }
+
+    Evaluation evaluation = evaluationRepository.findById(evaluationId)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Evaluación no encontrada"));
+
+    User user = userRepository.findById(userCi)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado"));
+
+    EvaluationSubmission submission = new EvaluationSubmission(
+      req.getSolution(),
+      fileName,
+      fileUrl,
+      null,
+      evaluation,
+      user
+    );
+
+    EvaluationSubmission saved = evaluationSubmissionRepository.save(submission);
+    return getDtEvaluationSubmission(saved);
+  }
 }

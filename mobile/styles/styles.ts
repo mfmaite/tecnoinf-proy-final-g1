7cfc441b0b520215a83c86ccb039f0c3bda5c8b8
Leo <leonardovieras@gmail.com>
2025-11-10 11:38:34 -0300

    estaba de nuevo justify content en styles, lo borré de nuevo, pero lo dejé comentado así no lo volvemos a agregar por error

--- mobile/styles/styles.ts
+++ mobile/styles/styles.ts
@@ -5,7 +5,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary[10],
-    justifyContent: "center",
+    // justifyContent: "center", NO USAR
     paddingTop: 10,
     paddingHorizontal: 20,
   },

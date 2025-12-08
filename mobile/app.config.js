034302a7f071f6aaec257041f71f55c678120109
Leo <leonardovieras@gmail.com>
2025-12-08 13:59:41 -0300

    casi casi tamo ahí pa la primer apk

---
+++ mobile/app.config.js
@@ -0,0 +1,59 @@
+import 'dotenv/config';
+
+export default ({ config }) => ({
+  ...config,
+  name: "Mentora",
+  slug: "mentora",
+  version: "1.0.0",
+  scheme: "mentora",
+  orientation: "portrait",
+  newArchEnabled: true,
+
+  android: {
+    ...config.android,
+    package: "com.mentora.mobile",
+
+    // 🔐 INYECTAMOS EL GOOGLE SERVICES DESDE SECRET, NO DESDE ARCHIVO LOCAL
+    googleServicesFile: process.env.EXPO_GOOGLE_SERVICES_JSON,
+
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png"
+    }
+  },
+
+  ios: {
+    ...config.ios,
+    bundleIdentifier: "com.mentora.mobile",
+    supportsTablet: true,
+    associatedDomains: ["applinks:mentora.app"]
+  },
+
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: { backgroundColor: "#000000" }
+      }
+    ],
+    "expo-secure-store"
+  ],
+
+  web: {
+    favicon: "./assets/images/favicon.ico",
+    output: "static"
+  },
+
+  // 🔑 ESTA ES LA MANERA CORRECTA DE EXPONER LA URL
+  extra: {
+    apiUrl: process.env.EXPO_PUBLIC_API_URL,
+    eas: { projectId: "6042f746-5d76-4884-bb96-d5c6febf9e72" }
+  }
+});

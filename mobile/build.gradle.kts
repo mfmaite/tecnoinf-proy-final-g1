394823f1c006e6ef19c5b16ce2e3985b6b6ba547
sebazx <sebastian08.jaime@gmail.com>
2025-12-17 21:08:25 -0300

    test mobile 1.2

---
+++ mobile/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+  id("com.android.application")
+
+  // Add the Google services Gradle plugin
+  id("com.google.gms.google-services")
+
+  }
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  // https://firebase.google.com/docs/android/setup#available-libraries
+}
